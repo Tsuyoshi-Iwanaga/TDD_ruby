1bed472445e107a836d1d4565108901df05983e8
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-22 02:20:37 +0900

    add minimum methods

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -4,5 +4,9 @@ require_relative 'bowling_game'
 class BowlingGameTest < MiniTest::Unit::TestCase
   def test_all_gutter_game
     game = BowlingGame.new
+    20.times do
+      game.record_shot(0)
+    end
+    assert_equal 0, game.score
   end
 end
