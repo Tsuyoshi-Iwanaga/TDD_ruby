25fffa715f96cef691bf5a01f9412e873082a8c0
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-22 03:14:27 +0900

    add testcase all get 1pin

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -9,4 +9,12 @@ class BowlingGameTest < MiniTest::Unit::TestCase
     end
     assert_equal 0, game.score
   end
+
+  def test_all_1pin_game
+    game = BowlingGame.new
+    20.times do
+      game.record_shot(1)
+    end
+    assert_equal 20, game.score
+  end
 end
