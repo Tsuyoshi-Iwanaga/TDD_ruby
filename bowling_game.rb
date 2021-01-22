e9102fe4077be3cc69e441ed3667d3f9bc96e0b8
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-23 01:16:11 +0900

    add testcase spare

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,10 +1,23 @@
 class BowlingGame
   def initialize
     @score = 0
+    @spare = false
+    @lastpins = 0
   end
 
   def record_shot(pins)
     @score += pins
+
+    if @spare
+      @score += pins
+      @spare = false
+    end
+
+    if @lastpins + pins == 10
+      @spare = true
+    end
+
+    @lastpins = pins
   end
 
   def score
--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -16,6 +16,14 @@ class BowlingGameTest < MiniTest::Unit::TestCase
     assert_equal 20, @game.score
   end
 
+  def test_get_spare
+    @game.record_shot(3)
+    @game.record_shot(7) #10 + 4 = 14
+    @game.record_shot(4)
+    record_many_shots(17, 0)
+    assert_equal 18, @game.score
+  end
+
   def record_many_shots(count, pins)
     count.times do
       @game.record_shot(pins)
