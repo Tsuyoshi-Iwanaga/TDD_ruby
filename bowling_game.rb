133f6c0b611628ff196d88da39577b47adc2fc14
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-23 02:12:30 +0900

    add testcase strike & implement logic

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,6 +4,7 @@ class BowlingGame
     @spare = false
     @lastpins = 0
     @shot_no = 1
+    @strike_bornus_count = 0
   end
 
   def record_shot(pins)
@@ -16,6 +17,15 @@ class BowlingGame
 
     if @shot_no == 2 && @lastpins + pins == 10
       @spare = true
+    end
+
+    if @strike_bornus_count > 0
+      @score += pins
+      @strike_bornus_count -= 1
+    end
+
+    if pins == 10
+      @strike_bornus_count = 2
     end
 
     @lastpins = pins
--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -33,6 +33,15 @@ class BowlingGameTest < MiniTest::Unit::TestCase
     assert_equal 13, @game.score
   end
 
+  def test_get_strike
+    @game.record_shot(10) #10 + 3 + 3
+    @game.record_shot(3)
+    @game.record_shot(3)
+    @game.record_shot(1)
+    record_many_shots(15, 0) #全体では15投になる
+    assert_equal 23, @game.score
+  end
+
   def record_many_shots(count, pins)
     count.times do
       @game.record_shot(pins)
