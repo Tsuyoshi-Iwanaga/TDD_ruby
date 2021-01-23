e223a024dca9904a12f4d2aba718f866bb62a4ae
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-01-23 14:42:07 +0900

    add test frame_finished?

--- frame.rb
+++ frame.rb
@@ -1,13 +1,19 @@
 class Frame
   def initialize
     @score = 0
+    @shot_count = 0
   end
 
   def record_shot(pins)
     @score += pins
+    @shot_count += 1
   end
 
   def score
     @score
+  end
+
+  def finished?
+    @shot_count > 1
   end
 end
--- frame_test.rb
+++ frame_test.rb
@@ -15,4 +15,12 @@ class FrameTest < MiniTest::Unit::TestCase
     frame.record_shot(1)
     assert_equal 2, frame.score
   end
+
+  def test_finished?
+    frame = Frame.new
+    frame.record_shot(1)
+    refute frame.finished?, "2投目が可能"
+    frame.record_shot(1)
+    assert frame.finished?, "2投したので終了"
+  end
 end
